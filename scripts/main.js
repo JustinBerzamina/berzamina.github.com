d28ac4e436d6c11c7896ded76ff691ef6d583bfa
JustinBerzamina <153406782+JustinBerzamina@users.noreply.github.com>
2024-02-06 13:21:54 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,45 @@
+const Iloveyoutoo = document.getElementById('button1');
+Iloveyoutoo.addEventListener('click', function() {
+    const storedName = localStorage.getItem("name");
+
+    if (storedName && ["Kylie", "Lie", "kylie", "lie"].includes(storedName)) {
+      alert('I LOVE YOU TOO BABY!!');
+    } else {
+      alert('Who the fuck are you?');
+    }
+  });
+
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/bebu.jpg") {
+    myImage.setAttribute("src", "images/bebu2.jpg");
+  } else {
+    myImage.setAttribute("src", "images/bebu.jpg");
+  }
+};
+
+let myButton = document.querySelector("#button3");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Do you know who my love is, ${myName}?`;
+  }
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Do you know who my love is, ${storedName}?`;
+}
+
+myButton.onclick = () => {
+  setUserName();
+};
